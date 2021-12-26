e74c3f521dbea4bdd41e362c62fa01a87f936f3c
Wugawuga <dnr3655@naver.com>
2021-12-26 22:14:15 +0900

    추가 - java 삼항연산자

---
+++ java_prac/src/chap05/CondOp.java
@@ -0,0 +1,21 @@
+package chap05;
+
+public class CondOp {
+
+	public static void main(String[] args) {
+		
+		int num1 = 50;
+		int num2 = 100;
+		int big;
+		int diff;
+		
+		big = (num1 > num2) ? num1 : num2;
+		
+		System.out.println("큰 수 : "+big);
+		
+		diff = (num1 > num2) ? (num1 - num2) : (num2 - num1);
+		
+		System.out.println("절댓값 : "+diff);
+	}
+
+}
