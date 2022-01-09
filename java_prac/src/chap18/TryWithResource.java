5cdf5db07ddb645ae0db85e94b64d05a8b0a8481
Wugawuga <dnr3655@naver.com>
2022-01-09 19:08:38 +0900

    추가 - 예외 처리

---
+++ java_prac/src/chap18/TryWithResource.java
@@ -0,0 +1,23 @@
+package chap18;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TryWithResource {
+
+	public static void main(String[] args) {
+		
+		Path file = Paths.get(null);
+		
+		try(BufferedWriter writer = Files.newBufferedWriter(file)){
+			writer.write('a');
+			writer.write('z');
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
