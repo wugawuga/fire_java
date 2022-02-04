2354c56f795bfd3bb1b9333e9ec45693610c7e50
Wugawuga <dnr3655@naver.com>
2022-02-04 23:19:34 +0900

    추가 - java 달팽이 알고리즘

---
+++ java_prac/src/quiz/Quiz22.java
@@ -0,0 +1,31 @@
+package quiz;
+
+import java.util.Scanner;
+
+public class Quiz22 {
+
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		System.out.print(">> ");
+		int a = sc.nextInt();
+		
+		for(int i=0;i<a;i++) {
+			for(int j=0;j<a;j++) {
+				int k = Math.min(j, i);
+				int b = Math.min(k, a-i-1);
+				int c = Math.min(b, a-j-1);
+				int d;
+				if(j>=i) {
+					d = i+j - c*2;
+				}else {
+					d = (a-1 - c*2)*4 - (i+j - c*2);
+				}
+				d += 4*(c*a-(c*c));
+				System.out.print(d+1+" ");
+			}
+			System.out.println();
+		}
+	}
+}
